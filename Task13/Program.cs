ce6dce91894b832cd8d2879c9d25d8469fcb6e45
Maria Yutaeva <mariya1982@yandex.ru>
2022-09-28 13:36:16 +0300

    задача 13

---
+++ Task13/Program.cs
@@ -0,0 +1,33 @@
+﻿int GetNumber()
+{
+    int num = 0;
+    while (LengthNum(num) <= 2)
+    {
+        Console.WriteLine("Введите число!");
+        num = Convert.ToInt32(Console.ReadLine());
+    }    
+    return num;
+}
+
+int LengthNum(int Number) // Получение количества цифр в числе
+{
+    int DigCount=0;
+    while(Math.Abs(Number)>0)
+    {
+        Number=Number/10;
+        DigCount++;
+    }
+    return DigCount;
+}
+
+int GetDigitFromNumber(int number, int index)
+{
+    int num = number/Convert.ToInt32(Math.Pow(10,LengthNum(number)-index))%10;
+    return num;
+}
+
+int num = GetNumber();
+Console.WriteLine("Вторая цифра: " + GetDigitFromNumber(num,2));
+Console.WriteLine("Третья цифра: " + GetDigitFromNumber(num,3));
+Console.WriteLine("Четвертая цифра: " + GetDigitFromNumber(num,4));
+Console.WriteLine("Пятая цифра: " + GetDigitFromNumber(num,5));
