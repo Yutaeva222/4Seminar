25094440c866282f31b8c655a77554dcb64e80ce
Maria Yutaeva <mariya1982@yandex.ru>
2022-09-28 12:22:50 +0300

    добавили задачу

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -13,4 +13,26 @@
 // Console.WriteLine(a);
 
 // написать программу, которая получает число А и находит сумму чисел до этого числа.
- 
+ int GetNumber()
+ {
+    int num = 0;
+    while (num <=0) // цикл проверяет число на положительность, пока будет вводиться 
+    // отрицательное, программа будет запрашивать ввести число заново. 
+    {
+       Console.WriteLine("Введите положительное число А: ");
+       num = Convert.ToInt32(Console.ReadLine());
+    }
+    return num;
+ }
+int GetSumm(int A)
+{
+    int summ = 0;
+    for (int i = 1; i <= A; i++)
+    {
+       summ = summ + i;
+    }
+    return summ;
+}
+ int A = GetNumber();
+ Console.WriteLine("Сумма чисел от 1 до " + A + " = " + GetSumm(A));
+
