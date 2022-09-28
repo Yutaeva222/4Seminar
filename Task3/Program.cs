c5f52334121ba15398f48097434a233870e11452
Maria Yutaeva <mariya1982@yandex.ru>
2022-09-28 13:49:44 +0300

    задача_факториал

---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿int GetNumber()
+{
+    Console.WriteLine("Введите число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetProizv(int number)
+{
+   int proizv = 1;
+   for (int i = 1; i <= number ; i++)
+   {
+    proizv = proizv * i;
+   }
+   return proizv;
+}
+int number = GetNumber();
+Console.WriteLine("Произведение чисел равно = " + GetProizv(number));
