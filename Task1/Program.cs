56c819ba7b3cad53b90e24b3bc3ae192d91b1a2a
Maria Yutaeva <mariya1982@yandex.ru>
2022-09-28 11:50:37 +0300

    задачи семинара

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// метод на введение трехзначного числа!!!
+// int GetNumberThreeDigit()
+// {
+//     int num = 0;
+//     while (num < 99 || num > 1000)
+//     {
+//         Console.WriteLine("Введите трехзначное число: ");
+//         num = Convert.ToInt32(Console.ReadLine());
+//     }
+//     return num;
+// }
+// int a = GetNumberThreeDigit();
+// Console.WriteLine(a);
+
+// написать программу, которая получает число А и находит сумму чисел до этого числа.
+ 
